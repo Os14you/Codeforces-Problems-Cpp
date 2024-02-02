a47f2e7007f569ccdb0886d647a3bc4f3f7a7afd
Os14you <os14you.manage@gmail.com>
2024-02-02 05:09:54 +0200

    Adding ICPC Templete File

---
+++ ECPC Compition 2023/ICPC Templete.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+	freopen("/filepath/txt/input.in","r",stdin);
+	freopen("/filepath/txt/output.out","w",stdout);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    //  Write your code here... 
+
+}
