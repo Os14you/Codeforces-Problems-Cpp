ae9588582da8fa4a2fac73235144d96540b878d9
Os14you <os14you.manage@gmail.com>
2024-03-04 18:37:09 +0200

    Adding one problem

--- Contests (Div. 2)/B. Binary Path/Solution.cpp
+++ Contests (Div. 2)/B. Binary Path/Solution.cpp
@@ -6,32 +6,25 @@
 #include <iostream>
 //#include <vector>
 using namespace std;
-char bool_to_char(bool val){
-    if(val)
-        return '1';
-    else
-        return '0';
-}
+
 int main(){
     int test_cases,columns,paths=1;
     cin>>test_cases;
     while(test_cases--){
         cin>>columns;
-        bool grid[2][columns];
-        for(int i=0;i<2;i++){
-            for(int j=0;j<columns;j++)
-                cin>>grid[i][j];
-        }
+        string grid[2];
+        for(int i=0;i<2;i++)
+            cin>>grid[i];
         string path="";
-        path+=bool_to_char(grid[0][0]);
+        path+=grid[0][0];
         for(int row=0,column=0;;){
-            if((column+1)==columns) break;
+            if(grid[0][column]=='\0') break;
             if(row == 1 || grid[row][column+1] < grid[row+1][column])
-                path+=bool_to_char(grid[row][column+1]),column++;
+                path+=grid[row][column+1],column++;
             else if(grid[row+1][column] < grid[row][column+1])
-                path+=bool_to_char(grid[row+1][column]),row++;
+                path+=grid[row+1][column],row++;
             else
-                path+=bool_to_char(grid[row][column+1]),column++,paths++;
+                path+=grid[row+1][column],row++,paths++;
         }
         cout<<path<<"\n"<<paths<<"\n";
     }
