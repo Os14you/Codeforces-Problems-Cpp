7bb62e8a582b1ec15ff82203c1c5b04129fe8be6
Os14you <os14you.manage@gmail.com>
2024-03-05 19:14:49 +0200

    Adding one problem

--- Dr. Mostafa Saad Sheet/Level A/A. Bear and Big Brother/Solution.cpp
+++ Dr. Mostafa Saad Sheet/Level A/A. Bear and Big Brother/Solution.cpp
@@ -4,7 +4,7 @@ int main(){
     int Limak,Bob;
     cin>>Limak>>Bob;
     int years=0;
-    while(Limak<Bob){ Limak*=3; Bob*=2; years++;}
+    while(Limak<=Bob){ Limak*=3; Bob*=2; years++;}
     cout<<years;
     return 0;
 }
