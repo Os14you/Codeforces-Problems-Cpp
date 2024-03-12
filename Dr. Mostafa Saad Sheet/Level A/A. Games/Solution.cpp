11e58c49c25e7576613e99f3b71fc583de67cff5
Os14you <os14you.manage@gmail.com>
2024-03-12 13:44:08 +0200

    Adding one problem

--- Dr. Mostafa Saad Sheet/Level A/A. Games/Solution.cpp
+++ Dr. Mostafa Saad Sheet/Level A/A. Games/Solution.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using std::cin,std::cout;
 int main(){
-    
+    int teams,result {};
+    cin>>teams;
+    int home[teams],away[teams];
+    for(int i=0;i<teams;++i)
+        cin>>home[i]>>away[i];
+    for(int i=0;i<teams;++i){
+        for(int j=0;j<teams;j++){
+            if(home[i]==away[j])
+                result++;
+        }
+    }
+    cout<<result;
 }
