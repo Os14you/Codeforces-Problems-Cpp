19f5e97d48b9a39a13a69981f0d7d4a007b1b0d8
Os14you <os14you.manage@gmail.com>
2024-03-23 07:52:14 +0200

    Adding solution of problem Domino piling

---
+++ Other/A. Domino piling/Solution.cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // link : https://codeforces.com/problemset/problem/50/A
+using std::cout,std::cin;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    cout<<(m*n)/2;
+    return 0;
+}
